b849db01567610639440a9293157a2155d6913a7
Edwin Agyemang <edwinoko@yahoo.com>
2024-01-18 12:33:16 +0100

    fixing build

--- components/experience.tsx
+++ components/experience.tsx
@@ -48,8 +48,8 @@ export default function Experience() {
               <h3 className="font-semibold capitalize">{item.company}</h3>
               <p className="font-normal !mt-0">{item.position}</p>
 
-              {item.summary.map((entry) => (
-              <div>
+              {item.summary.map((entry, index) => (
+              <div key={index}>
                 <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
                   {entry}
                 </p>
